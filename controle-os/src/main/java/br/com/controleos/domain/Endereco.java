133899f8bf1063bf94cf39bc7ff7eb3d3a832512
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-10-02 23:30:44 -0300

    Modificado tipo da entidade para Embeddable

--- controle-os/src/main/java/br/com/controleos/domain/Endereco.java
+++ controle-os/src/main/java/br/com/controleos/domain/Endereco.java
@@ -1,129 +1,39 @@
 package br.com.controleos.domain;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.Embeddable;
 
-@Entity
-@Table(name = "endereco")
+import lombok.Data;
+
+@Embeddable
+@Data
 public class Endereco {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "codigo")
-	private Long codigo;
-
-	@Column(name = "cep")
-	private String cep;
-
-	@Column(name = "logradouro")
+	@Column(length = 50)
 	private String logradouro;
 
-	@Column(name = "bairro")
-	private String bairro;
+	@Column(length = 6)
+	private Integer numero;
 
-	@Column(name = "numero")
-	private String numero;
-
-	@Column(name = "cidade")
-	private String cidade;
-
-	@Column(name = "uf")
-	private String uf;
-
-	@Column(name = "complemento")
+	@Column(length = 50)
 	private String complemento;
 
-	public Long getCodigo() {
-		return codigo;
-	}
+	@Column(length = 50)
+	private String bairro;
 
-	public void setCodigo(Long codigo) {
-		this.codigo = codigo;
-	}
+	@Column(length = 10)
+	private String cep;
 
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getLogradouro() {
-		return logradouro;
-	}
-
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getUf() {
-		return uf;
-	}
-
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
-
-	public String getComplemento() {
-		return complemento;
+	public void setLogradouro(String logadouro) {
+		this.logradouro = logadouro.toUpperCase();
 	}
 
 	public void setComplemento(String complemento) {
-		this.complemento = complemento;
+		this.complemento = complemento.toUpperCase();
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Endereco other = (Endereco) obj;
-		if (codigo == null) {
-			if (other.codigo != null)
-				return false;
-		} else if (!codigo.equals(other.codigo))
-			return false;
-		return true;
+	public void setBairro(String bairro) {
+		this.bairro = bairro.toUpperCase();
 	}
 
 }
