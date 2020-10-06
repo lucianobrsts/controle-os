524aeb6a2be51c87ad2a718732a92c303c5cb55f
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-10-05 21:22:31 -0300

    Removido para reestruturacao do projeto

--- controle-os/src/main/java/br/com/controleos/dao/ClienteDAO.java
+++
@@ -1,7 +0,0 @@
-package br.com.controleos.dao;
-
-import br.com.controleos.domain.Cliente;
-
-public class ClienteDAO extends GenericDAO<Cliente> {
-
-}
--- controle-os/src/main/java/br/com/controleos/dao/EnderecoDAO.java
+++
@@ -1,7 +0,0 @@
-package br.com.controleos.dao;
-
-import br.com.controleos.domain.Endereco;
-
-public class EnderecoDAO extends GenericDAO<Endereco> {
-
-}
